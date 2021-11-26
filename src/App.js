9aebcff2cf2c72d33021a1f550858af973f71a8e
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-11-26 15:20:10 +0100

    Included h1 and footer with link to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Katharina Weberberger and is{" "}
+        <a
+          href="https://github.com/katharinaweb/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://inspiring-austin-a0a9ea.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
